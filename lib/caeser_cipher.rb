4c3cb9aa511df39efc18d7ab72f2d47ef960a778
cjdepo <cjdepowell@protonmail.com>
2021-12-09 22:10:41 -0500

    tests complete

--- lib/caeser_cipher.rb
+++ lib/caeser_cipher.rb
@@ -5,18 +5,22 @@ def caeser_cipher(string, shift)
     alphabet_array = alphabet.split("")
     string_array = string.split("")
     new_string_array = string_array.map do |letter| 
-        new_index = alphabet_array.find_index(letter.downcase) + shift
-        if new_index > 25
-            new_index -= 26
-        end
-        if !alphabet_array.include?(letter)
-            alphabet_array[new_index].upcase
+        if letter == ' '
+            ' '
         else
-            alphabet_array[new_index]
+            new_index = alphabet_array.find_index(letter.downcase) + shift
+            while new_index > 25
+                new_index -= 26
+            end
+            if alphabet_array.include?(letter)
+                alphabet_array[new_index]
+            elsif alphabet_array.map{ |letter| letter.upcase }.include?(letter)
+                alphabet_array[new_index].upcase
+            end
         end
     end
     new_string_array.join
 
 end
 
-p caeser_cipher("helloallDFDSFLKJslkjdfzzzzzzzzzz", 5)
+p caeser_cipher("helloallDFDSFL KJslkjdfzzzzzzzzzz", 5)
--- spec/caeser_cipher_spec.rb
+++ spec/caeser_cipher_spec.rb
@@ -2,8 +2,27 @@
 require './lib/caeser_cipher.rb'
 
 describe "#caeser_cipher" do
-    it "returns cipher for single word five shifts" do
+    it "returns cipher for single word" do
         expect(caeser_cipher("goodbye", 5)).to eql("lttigdj")
+    end
+    it "returns cipher for multiple words" do
+        expect(caeser_cipher("good bye", 5)).to eql("ltti gdj")
+    end
+
+    it "returns cipher for all shift > 26" do
+        expect(caeser_cipher("good bye", 31)).to eql("ltti gdj")
+    end
+
+    it "works for capital letters" do
+        expect(caeser_cipher("GOOD bye", 5)).to eql("LTTI gdj")
+    end
+
+    it "works for negative shift > -26" do
+        expect(caeser_cipher("good bye", -1)).to eql("fnnc axd")
+    end
+
+    it "works for negative shift < -26" do
+        expect(caeser_cipher("good bye", -27)).to eql("fnnc axd")
     end
 end
     
